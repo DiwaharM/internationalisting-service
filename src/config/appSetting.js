4612815c5bc2db5db82ecfa01b6e4597193e64ca
Diwahar <ripsildeveloper4@gmail.com>
2019-06-10 11:07:11 +0530

    changes in appSetting

--- src/config/appSetting.js
+++ src/config/appSetting.js
@@ -1,5 +1,5 @@
 module.exports = {
-    adsUploadPath: 'http://localhost/listing/ads',
+    /* adsUploadPath: 'http://localhost/listing/ads',
     adsServerPath: 'http://localhost/listing/ads/',
     bannerUploadPath: 'http://localhost/listing/banner',
     bannerServerPath: 'http://localhost/listing/banner/',
@@ -9,10 +9,10 @@ module.exports = {
     footerServerPath: 'http://localhost/listing/footer/',
     businessUserUploadPath: './images/businessuser',
     businessUserServerPath: 'http://localhost/list/businessuser/',
-    categoryServerPath: "http://localhost/listing/category/",
+    categoryServerPath: "http://localhost/listing/category/", */
 
 
-/*     adsUploadPath: '/home/ubuntu/app/listing-admin-ui/listing-admin-panel/images/ads',
+    adsUploadPath: '/home/ubuntu/app/listing-admin-ui/listing-admin-panel/images/ads',
     adsServerPath: 'http://internationalstandardlisting.com/admin/images/ads/',
     bannerUploadPath: '/home/ubuntu/app/listing-admin-ui/listing-admin-panel/images/banner',
     bannerServerPath: 'http://internationalstandardlisting.com/admin/images/banner/',
@@ -22,5 +22,5 @@ module.exports = {
     footerServerPath: 'http://internationalstandardlisting.com/admin/images/footer/',
     businessUserUploadPath: '/home/ubuntu/app/listing-admin-ui/listing-admin-panel/images/businessuser',
     businessUserServerPath: 'http://internationalstandardlisting.com/admin/images/businessuser/',
-    categoryServerPath: "http://internationalstandardlisting.com/admin/images/category/", */
+    categoryServerPath: "http://internationalstandardlisting.com/admin/images/category/",
 }
